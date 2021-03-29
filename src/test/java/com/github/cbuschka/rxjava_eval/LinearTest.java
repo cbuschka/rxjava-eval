2686a59b980c37f8753dcdf876df1519035fc908
conni <cbuschka@gmail.com>
2021-03-29 10:03:21 +0200

    Replaced examples with real tests.

--- src/test/java/com/github/cbuschka/rxjava_eval/LinearTest.java
+++ src/test/java/com/github/cbuschka/rxjava_eval/LinearTest.java
@@ -3,18 +3,34 @@ package com.github.cbuschka.rxjava_eval;
 import io.reactivex.Observable;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LinearTest
 {
+	private final List<String> output = new ArrayList<>();
+
+	@Test
+	void single()
+	{
+		Observable<String> observer = Observable.just("Hello world!");
+		observer.subscribe(output::add).dispose();
+
+		assertThat(output).containsExactly("Hello world!");
+	}
+
 	@Test
 	void linearPrintln()
 	{
 		List<String> messages = Arrays.asList("Hello", "World", "!");
 
 		Observable.fromIterable(messages)
-				.subscribe((m) -> System.err.println("Subscriber: " + m))
+				.subscribe((m) -> output.add("Subscriber: " + m))
 				.dispose();
+
+		assertThat(output).containsExactly("Subscriber: Hello", "Subscriber: World", "Subscriber: !");
 	}
 }
